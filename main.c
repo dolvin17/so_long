d88e6856ece995caddde81dd99b7e28aaa9d7301
ghuertas <ghuertas@student.42madrid.com>
2022-09-24 17:33:19 +0200

    gracias Israaaa

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+
 #include "mlx.h"
+
+int	render_next_frame(void *YourStruct);
 
 int	main(void)
 {
 	void	*mlx;
 
 	mlx = mlx_init();
-	return (0);
+	mlx_new_window(mlx, 300, 300, "funciona");
+	mlx_loop(mlx);
 }
