fd9e80a398150cb704834c19a7d3bc995e88e94e
dolvin17 <huertas876@gmail.com>
2022-09-26 17:33:36 +0200

    puting a red pixel on window

--- main.c
+++ main.c
@@ -1,13 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "mlx.h"
 
-int	render_next_frame(void *YourStruct);
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 600
+
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+#define XK_Escape 0xFF1B
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	/* if window has been destroyed, we don't want to put the pixel ! */
+	if (data->win_ptr != NULL)
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
+			WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_new_window(mlx, 300, 300, "funciona");
-	mlx_loop(mlx);
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	/* Setup hooks */ 
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_loop(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
